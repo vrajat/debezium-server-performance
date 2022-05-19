35f71d9b23c579e81767bd9fea03102510af5b51
Rajat Venkatesh <1638298+vrajat@users.noreply.github.com>
2022-05-19 12:57:58 +0530

    Fix stream delete. Add multiple server option

--- connector_setup.sh
+++ connector_setup.sh
@@ -10,8 +10,10 @@ fi
 
 export PATH=$PATH:$YBPATH
 
+if [[ -n "$CDC_SDK_STREAM_ID" ]]; then
+    yb-admin --master_addresses $MASTER_ADDRESSES delete_change_data_stream $CDC_SDK_STREAM_ID
+fi
 ysqlsh -f ysql_drop_tables.sql
-yb-admin --master_addresses $MASTER_ADDRESSES delete_change_data_stream $CDC_SDK_STREAM_ID
 
 ysqlsh -f ysql_schema.sql 
 yb-admin create_change_data_stream ysql.yugabyte --master_addresses $MASTER_ADDRESSES
